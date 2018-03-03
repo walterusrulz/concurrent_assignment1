06560e5d199da06380ae92b12ba17e9755dc8199
walterusrulz <>
2018-03-03 17:21:53 +0100

    Package 1.A functioning.

--- A/Reference.java
+++ A/Reference.java
@@ -4,5 +4,17 @@ package concurrent_assignment1.A;
  * Get the reference of the Main Thread */
 
 public class Reference {
-
+   public static void main(String args[]){
+       int oddNum = -1;
+       
+       Thread muThread = Thread.currentThread();
+       System.out.println("Original thread name: "+muThread.getName());
+       String nameVanilla = muThread.getName();
+       for(int i=0; i<50;i++){
+            muThread.setName(nameVanilla+(i+1));
+            oddNum += 2;
+            System.out.println("Odd number "+(i+1)+" is: "+oddNum+" ->"+muThread.getName());
+        }
+    } 
+   
 }
