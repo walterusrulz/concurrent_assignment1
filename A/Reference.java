96a7be88f74f33739a85e10f0111102202b43cbb
walterus <walterus@walterus-HP-EliteBook-2560p>
2019-02-19 12:42:02 +0100

    Sleep added to 1A

--- A/Reference.java
+++ A/Reference.java
@@ -1,5 +1,8 @@
 package concurrent_assignment1.A;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Get the reference of the Main Thread */
 
@@ -13,6 +16,11 @@ public class Reference {
        for(int i=0; i<50;i++){
             muThread.setName(nameVanilla+(i+1));
             oddNum += 2;
+           try {
+               Thread.sleep(2000);
+           } catch (InterruptedException ex) {
+               Logger.getLogger(Reference.class.getName()).log(Level.SEVERE, null, ex);
+           }
             System.out.println("Odd number "+(i+1)+" is: "+oddNum+" ->"+muThread.getName());
         }
     } 
