f05313b47ebf968ea42a4ae15a7bd3b1d90ca49b
Onsurbex <onsurbex@gmail.com>
2019-02-19 17:44:31 +0100

    B: Exercise Done

--- B/T_PrintID.java
+++ B/T_PrintID.java
@@ -8,6 +8,17 @@ package concurrent_assignment1.B;
  * @author yournamehere
  */
 
-public class T_PrintID {
-
+public class T_PrintID extends Thread{
+    int ID;
+    
+    public T_PrintID(int ID){
+        this.ID = ID;
+    }
+    
+    @Override
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": ID is " + this.ID);
+        }
+    }
 }
